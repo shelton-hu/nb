03481fe68c7ebd256bc6754bca5164d9a1489c10
shelton-hu <hujiahui1993@163.com>
2021-04-29 15:44:03 +0800

    init

---
+++ functrace/trace.go
@@ -0,0 +1,26 @@
+package functrace
+
+import (
+	"context"
+	"runtime"
+
+	microTrace "github.com/micro/go-plugins/wrapper/trace/opentracing/v2"
+	opentracing "github.com/opentracing/opentracing-go"
+)
+
+func Trace(ctx context.Context) {
+	name, file, line := runFunc()
+	_, span, _ := microTrace.StartSpanFromContext(ctx, opentracing.GlobalTracer(), name)
+	defer span.Finish()
+	span.LogKV("file", file)
+	span.LogKV("line", line)
+}
+
+func runFunc() (name string, file string, line int) {
+	pc := make([]uintptr, 1)
+	runtime.Callers(2, pc)
+	f := runtime.FuncForPC(pc[0])
+	name = f.Name()
+	file, line = f.FileLine(pc[0])
+	return
+}
